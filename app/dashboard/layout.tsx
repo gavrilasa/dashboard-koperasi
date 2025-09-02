17421f4526f34946f80f55e6b920add7f34618cc
gavrilasa <gavrilasamanaa@gmail.com>
2025-09-02 20:26:56 +0700

    docs(boilerplate):Initial File

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+// path: app/dashboard/layout.tsx
+
+import Header from "@/app/components/layout/header";
+import Sidebar from "@/app/components/layout/sidebar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Dasbor Koperasi",
+	description: "Aplikasi Manajemen Koperasi",
+};
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<>
+			<Header />
+			<div className="flex h-screen overflow-hidden">
+				<Sidebar />
+				<main className="w-full pt-16">{children}</main>
+			</div>
+		</>
+	);
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// path: app/dashboard/page.tsx
+
+export default function DashboardPage() {
+	return (
+		<div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+			<div className="flex items-center justify-between space-y-2">
+				<h2 className="text-3xl font-bold tracking-tight">
+					Selamat Datang di Dasbor 👋
+				</h2>
+			</div>
+			<p>Ini adalah halaman utama dasbor Anda.</p>
+		</div>
+	);
+}
